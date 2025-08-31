d6c7e393bfe1a76012b12c5d3959ae4f838db87c
dachever <36123655+dachever@users.noreply.github.com>
2025-09-01 01:34:40 +0800

    Add files via upload

---
+++ Models/ContactFormModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyPortfolio.Models
+{
+    public class ContactFormModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Subject is required")]
+        public string? Subject { get; set; }
+
+        [Required(ErrorMessage = "Message is required")]
+        public string? Message { get; set; }
+    }
+}
