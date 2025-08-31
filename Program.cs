668091689cc9721218b4db1e9eb6908b0de964df
dachever <36123655+dachever@users.noreply.github.com>
2025-09-01 01:05:07 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+// ✅ Make sure app listens on port 80 inside Docker
+app.Urls.Add("http://+:80");
+
+app.Run();
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-My final Test
+# MyPortfolio
